896a95f43c536e7339c41db6e25075bc0606dc15
Pratham Ravindra Nagpure <pratham@localhost.localdomain>
2023-01-03 16:31:17 +0530

    added dark mode button

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const submit_btn = document.getElementById("submit-btn");
 const list = document.getElementById("list");
 const clear_all_btn = document.getElementById("clear-all-btn");
 const text_input = document.getElementById("text-input");
-
+const mode_btn = document.getElementById("mode-btn");
+const body = document.querySelector("body");
+const title = document.getElementById("title");
+let dark_mode =  true
 
 text_input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
@@ -21,4 +24,21 @@ clear_all_btn.addEventListener("click", function(){
     list.innerHTML = "";
 });
 
-
+mode_btn.addEventListener("click",function(){
+    if(dark_mode){
+        body.style.backgroundColor = "lightblue";
+        list.style.color = "black";
+        mode_btn.textContent = "dark mode";
+        title.style.color = "black";
+        text_input.style.backgroundColor = "white";
+        dark_mode = false;
+    }else{
+        body.style.backgroundColor = "black";
+        list.style.color = "lightblue";
+        mode_btn.textContent = "light mode";
+        title.style.color = "gray";
+        text_input.style.backgroundColor = "gray";
+        dark_mode = true
+    }
+    
+});
